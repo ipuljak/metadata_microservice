f3d90afc3ef8459c4f66fd5ed9fc2722ed8e52be
Ivan <ivanpulj@gmail.com>
2017-01-31 04:04:03 -0500

    Removed database requirement

--- routes/metadata.js
+++ routes/metadata.js
@@ -1,20 +1,10 @@
 const express = require('express'),
   moment = require('moment'),
-  mysql = require('mysql'),
   axios = require('axios'),
   multer = require('multer'),
-  secrets = require('../config'),
   router = express.Router();
 
 const upload = multer({ dest: 'uploads/' });
-
-// Connect to the mysql database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'image_user',
-  password: secrets.password,
-  database: 'image_search'
-});
 
 /**
  *  POST route to upload file to the server. 
